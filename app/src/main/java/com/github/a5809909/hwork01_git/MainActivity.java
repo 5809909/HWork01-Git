8fb70c4f49c80a5a73773fbc01b73fb45bf42fac
Victor <5809909@tut.by>
2017-10-04 18:51:24 +0300

    Initial commit

---
+++ app/src/main/java/com/github/a5809909/hwork01_git/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.a5809909.hwork01_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
